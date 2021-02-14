8a82abd2df2707e9a8188dff6c915171ef10e67e
Abhi2080 <57823658+Abhi2080@users.noreply.github.com>
2021-02-14 13:11:30 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,6 +10,10 @@ nvm install node
 
 npm install
 
+sudo apt install npm
+
+sudo apt install nodejs
+
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
